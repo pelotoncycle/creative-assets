9ba469457304f61885324eb4b516a7c0cd11e85c
Danny Feinberg <dannyfeinberg@MacBook-Pro-3.local>
2018-08-02 11:15:34 -0400

    Fix facebook share platforms

--- constants/fb_share_floor.js
+++ constants/fb_share_floor.js
@@ -3,61 +3,61 @@ const fb_share_floor = [
     name: "Becs Gentry",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/c511a3e9b15a4cd8bbeb61fd72ab6e3a",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   },
   {
     name: "Colleen Saidman Yee",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/d9e608a0c7654afe95c8384281455095",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   },
   {
     name: "Robin Arzon",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/970c398ecfc246a48b164985c09f6f18",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   },
   {
     name: "Chase Tucker",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/aeeca686867446839b7a9ce2f0c2a8ae",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   },
   {
     name: "Andy Speer",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/aa31a96fc9f749f5970f380601254212",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   },
   {
     name: "Olivia Amato",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/0e01d27b8266439285a3f1d9d39f815c",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   },
   {
     name: "Matty Maggiacomo",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/20f4116798764dd2bbf890fec7a5356a",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   },
   {
     name: "Rebecca Kennedy",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/fc8e62481d804316a5342a84f7f2a91c",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   },
   {
     name: "Selena Samuela",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/ae1b787cd8d54d9b830580318c3c4768",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   },
   {
     name: "Oliver Lee",
     image: "https://workout-metric-images-prod.s3.amazonaws.com/da62894a50834854b9ab01529526ba1d",
     source: 's3',
-    platforms: ['bike-tread, ios']
+    platforms: ['bike-tread', 'ios', 'web']
   }
 ]
 
